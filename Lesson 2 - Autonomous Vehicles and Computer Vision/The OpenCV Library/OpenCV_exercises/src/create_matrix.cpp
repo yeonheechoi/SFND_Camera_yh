6f4ba0fecc43013d41ef6e9bfa6d606a05d0d392
yeonheechoi <yonnii27@gmail.com>
2021-10-18 09:52:16 +0000

    short task_Create m3_8u

--- Lesson 2 - Autonomous Vehicles and Computer Vision/The OpenCV Library/OpenCV_exercises/src/create_matrix.cpp
+++ Lesson 2 - Autonomous Vehicles and Computer Vision/The OpenCV Library/OpenCV_exercises/src/create_matrix.cpp
@@ -10,22 +10,32 @@ void createMatrix1()
     // create matrix
     int nrows = 480, ncols = 640;
     cv::Mat m1_8u;
+
     m1_8u.create(nrows, ncols, CV_8UC1); // two-channel matrix with 8bit unsigned elements
-    m1_8u.setTo(cv::Scalar(0));        // white 255->0 test
+    m1_8u.setTo(cv::Scalar(255));        // white 255->0 test
 
     // STUDENT TASK :
     // Create a variable of type cv::Mat* named m3_8u which has three channels with a
     // depth of 8bit per channel. Then, set the first channel to 255 and display the result.
+    cv::Mat m3_8u;
 
+    m3_8u.create(nrows, ncols, CV_8UC3);//C1 means grayscale (1 Channel), C3 means RGB Color (3 Channel)
+    m3_8u.setTo(cv::Scalar(255, 0, 0));
 
+    /*
     // show result
     string windowName = "First steps in OpenCV (m1_8u)";
     cv::namedWindow(windowName, 1); // create window
     cv::imshow(windowName, m1_8u);
     cv::waitKey(0); // wait for keyboard input before continuing
+    */
 
     // STUDENT TASK :
     // Display the results from the STUDENT TASK above
+    string windowName = "First steps in OpenCV (m3_8u)";
+    cv::namedWindow(windowName, 1); // create window
+    cv::imshow(windowName, m3_8u);
+    cv::waitKey(0); // wait for keyboard input before continuing
 
 }
 
