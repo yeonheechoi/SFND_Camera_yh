d0d477f7ceb170d8f3cc5e3f399aee5e5e640241
yeonheechoi <yonnii27@gmail.com>
2021-10-28 11:28:32 +0000

    sobel xy update

--- Lesson 4 - Tracking Image Features/Intensity Gradient and Filtering/gradient_filtering/src/gradient_sobel.cpp
+++ Lesson 4 - Tracking Image Features/Intensity Gradient and Filtering/gradient_filtering/src/gradient_sobel.cpp
@@ -21,28 +21,7 @@ void gradientSobel()
     cv::Mat imgGray;
     cv::cvtColor(img, imgGray, cv::COLOR_BGR2GRAY);
 
-    //guassian smoothing
-        // create filter kernel
-    float gauss_data[25] = {1, 4, 7, 4, 1,
-                            4, 16, 26, 16, 4,
-                            7, 26, 41, 26, 7,
-                            4, 16, 26, 16, 4,
-                            1, 4, 7, 4, 1};
-
-        // slove Gaussian smoothing code error
-    for (int i = 0; i < 25; i++)
-    {
-        auto gauss_sum = accumulate(gauss_data, gauss_data+25, 0);
-        gauss_data[i] /= gauss_sum;
-    }
-
-    cv::Mat kernel_gaussian = cv::Mat(5, 5, CV_32F, gauss_data);
-
-
-        // apply filter
-    cv::Mat result_gaussian;
-    cv::filter2D(imgGray, result_gaussian, -1, kernel_gaussian, cv::Point(-1, -1), 0, cv::BORDER_DEFAULT);
-
+ 
     // create sobel filter kernel
     float sobel_x[9] = {-1, 0, +1,
                         -2, 0, +2, 
@@ -60,7 +39,7 @@ void gradientSobel()
     // apply filter
     cv::Mat result_x;
     cv::Mat result_xy;
-    cv::filter2D(result_gaussian, result_x, -1, kernel_x, cv::Point(-1, -1), 0, cv::BORDER_DEFAULT);
+    cv::filter2D(imgGray, result_x, -1, kernel_x, cv::Point(-1, -1), 0, cv::BORDER_DEFAULT);
     cv::filter2D(result_x, result_xy, -1, kernel_y, cv::Point(-1, -1), 0, cv::BORDER_DEFAULT);
 
     // show result
