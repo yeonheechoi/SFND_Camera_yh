e5db01fcb48e24d22aac95e155e14afb66c885c2
yeonheechoi <yonnii27@gmail.com>
2021-10-30 02:00:46 +0000

    harris detector

--- Lesson 4 - Tracking Image Features/Harris Corner Detection/cornerness_harris/src/cornerness_harris.cpp
+++ Lesson 4 - Tracking Image Features/Harris Corner Detection/cornerness_harris/src/cornerness_harris.cpp
@@ -24,20 +24,66 @@ void cornernessHarris()
     cv::Mat dst, dst_norm, dst_norm_scaled;
     dst = cv::Mat::zeros(img.size(), CV_32FC1);
     cv::cornerHarris(img, dst, blockSize, apertureSize, k, cv::BORDER_DEFAULT);
+    // (입력이미지, 출력이미지, 인접 픽셀 크기, Sobel Ksize, Harris parameter, 픽셀 보간법)
     cv::normalize(dst, dst_norm, 0, 255, cv::NORM_MINMAX, CV_32FC1, cv::Mat());
+    // 정규화 (입력 이미지, 출력 이미지, normalize range(lovw), normalize range(high), 픽셀 보간법)
     cv::convertScaleAbs(dst_norm, dst_norm_scaled);
+    // 미분한 결과에 절대값을 적용해서 8 bit unsigned int 형으로 바꿈
 
     // visualize results
-    string windowName = "Harris Corner Detector Response Matrix";
-    cv::namedWindow(windowName, 4);
-    cv::imshow(windowName, dst_norm_scaled);
-    cv::waitKey(0);
+    //string windowName = "Harris Corner Detector Response Matrix";
+    //cv::namedWindow(windowName, 4);
+    //cv::imshow(windowName, dst_norm_scaled);
+    //cv::waitKey(0);
 
     // TODO: Your task is to locate local maxima in the Harris response matrix 
     // and perform a non-maximum suppression (NMS) in a local neighborhood around 
     // each maximum. The resulting coordinates shall be stored in a list of keypoints 
     // of the type `vector<cv::KeyPoint>`.
+    double maxOverlap = 0.0;
+    vector<cv::KeyPoint> keypoints;
 
+        for (int r = 0; r < dst_norm.rows; r++)
+    {
+        for (int c = 0; c < dst_norm.cols; c++)
+        {
+            int response = (int)dst_norm.at<float>(r, c);
+            if (response > minResponse)
+            { 
+                cv::KeyPoint newKeyPoint;
+                newKeyPoint.pt = cv::Point2f(r, c);
+                newKeyPoint.size = 2 * apertureSize;
+                newKeyPoint.response = response;
+
+                // perform non-maximum suppression (NMS) in local neighbourhood around new key point
+                bool Overlap = false;
+                for (auto it = keypoints.begin(); it != keypoints.end(); ++it)
+                {
+                    double kptOverlap = cv::KeyPoint::overlap(newKeyPoint, *it);
+                    if (kptOverlap > maxOverlap)
+                    {
+                        Overlap = true;
+                        if (newKeyPoint.response > (*it).response)
+                        {                      // if overlap is >t AND response is higher for new kpt
+                            *it = newKeyPoint; // replace old key point with new one
+                            break;             // quit loop over keypoints
+                        }
+                    }
+                }
+                if (!Overlap)
+                {                                     // only add new key point if no overlap has been found in previous NMS
+                    keypoints.push_back(newKeyPoint); // store new keypoint in dynamic list
+                }
+            }
+        } 
+    }     
+
+    string windowName = "Harris Corner Detection Results";
+    cv::namedWindow(windowName, 5);
+    cv::Mat visImage = dst_norm_scaled.clone();
+    cv::drawKeypoints(dst_norm_scaled, keypoints, visImage, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+    cv::imshow(windowName, visImage);
+    cv::waitKey(0);
 }
 
 int main()
