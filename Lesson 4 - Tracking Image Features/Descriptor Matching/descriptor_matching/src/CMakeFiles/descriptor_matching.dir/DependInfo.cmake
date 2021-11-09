e3164c9cfb832683748fd14bdbc534ba18d554b7
yeonheechoi <yonnii27@gmail.com>
2021-11-09 11:03:30 +0000

    Descriptor matching

---
+++ Lesson 4 - Tracking Image Features/Descriptor Matching/descriptor_matching/src/CMakeFiles/descriptor_matching.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/SFND_Camera_yh/Lesson 4 - Tracking Image Features/Descriptor Matching/descriptor_matching/src/descriptor_matching.cpp" "/root/SFND_Camera_yh/Lesson 4 - Tracking Image Features/Descriptor Matching/descriptor_matching/src/CMakeFiles/descriptor_matching.dir/descriptor_matching.cpp.o"
+  "/root/SFND_Camera_yh/Lesson 4 - Tracking Image Features/Descriptor Matching/descriptor_matching/src/structIO.cpp" "/root/SFND_Camera_yh/Lesson 4 - Tracking Image Features/Descriptor Matching/descriptor_matching/src/CMakeFiles/descriptor_matching.dir/structIO.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Lesson 4 - Tracking Image Features/Descriptor Matching/descriptor_matching/src/CMakeFiles/descriptor_matching.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/descriptor_matching.dir/descriptor_matching.cpp.o"
+  "CMakeFiles/descriptor_matching.dir/structIO.cpp.o"
+  "descriptor_matching.pdb"
+  "descriptor_matching"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/descriptor_matching.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
